5133ad5025c77e9c0c479afecfe2a40c9769cc68
Minh Dinh <minhdv.dev@gmail.com>
2025-03-26 16:46:46 +0700

    chore: Add ESLint and Prettier configuration, update Angular settings, and enhance styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts,css,scss}"
-  ],
+  content: ["./src/**/*.{html,ts,css,scss}"],
   theme: {
     extend: {
       container: {
         center: true,
         padding: {
-          DEFAULT: '16px',
-          md: '32px',
-          xl: '80px'
+          DEFAULT: "16px",
+          md: "32px",
+          xl: "80px",
         },
         screens: {
-          sm: '640px',
-          md: '768px',
-          lg: '1024px',
-          xl: '1280px',
-          '2xl': '1536px'
-        }
-      }
-    }
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1536px",
+        },
+      },
+    },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio')
-  ]
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
