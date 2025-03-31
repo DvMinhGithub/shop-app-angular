312154dad84f3715857237f2af3cfffa01cdbbe7
Minh Dinh <minhdv.dev@gmail.com>
2025-03-31 21:10:04 +0700

    feat: Add MainLayoutComponent, update routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,27 @@
-import { RouterModule, ROUTES, Routes } from '@angular/router'
-import { HomeComponent } from './pages/home/home.component'
-import { LoginComponent } from './pages/login/login.component'
-import { OrderDetailComponent } from './pages/order-detail/order-detail.component'
-import { OrderComponent } from './pages/order/order.component'
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component'
-import { RegisterComponent } from './pages/register/register.component'
-import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { OrderDetailComponent } from './pages/order-detail/order-detail.component';
+import { OrderComponent } from './pages/order/order.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NgModule } from '@angular/core';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'product/:id', component: ProductDetailComponent },
-  { path: 'order', component: OrderComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'order', component: OrderComponent },
+      { path: 'order-detail/:id', component: OrderDetailComponent }
+    ]
+  },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'order-detail/:id', component: OrderDetailComponent }
-]
-
+  { path: 'login', component: LoginComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
