5237cedbafaa5cc672f0ecc9ab03627eae877db7
Minh Dinh <minhdv.dev@gmail.com>
2025-04-02 10:07:57 +0700

    feat: Add order management function for Admin

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,8 +2,9 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { headersApi, orderApi } from '../contant/api'
-import { IOrderCreateRequest, IOrderResponse } from '../types/order'
+import { IOrderCreateRequest, IOrderResponse, TPaginatedOrderResponse } from '../types/order'
 import { IApiResponse } from '../types/response'
+import { IPagination } from '../types/request'
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,9 @@ export class OrderService {
 
   getOrder(orderId: number): Observable<IApiResponse<IOrderResponse>> {
     return this.http.get<IApiResponse<IOrderResponse>>(orderApi.getOrder(orderId), { headers: headersApi() })
+  }
+
+  getOrdersByKey(key: string, pagi: IPagination): Observable<IApiResponse<TPaginatedOrderResponse>> {
+    return this.http.get<IApiResponse<TPaginatedOrderResponse>>(orderApi.getOrdersByKey(key, pagi), { headers: headersApi() })
   }
 }
--- src/app/types/response.ts
+++ src/app/types/response.ts
@@ -3,3 +3,32 @@ export interface IApiResponse<T> {
   message: string
   result: T
 }
+
+export interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  sort: Sort;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+export interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+export interface IPaginatedResponse<T> {
+  content: T[];
+  pageable: Pageable;
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  first: boolean;
+  size: number;
+  number: number;
+  sort: Sort;
+  numberOfElements: number;
+  empty: boolean;
+}
