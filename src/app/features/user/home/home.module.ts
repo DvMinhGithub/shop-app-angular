7fa5bc45577a1ff29a29b9431b6b001e0bc886b2
Minh Dv <minhdv.dev@gmail.com>
2026-01-05 22:38:33 +0700

    feat: implement user home page

--- src/app/features/user/home/home.module.ts
+++ src/app/features/user/home/home.module.ts
@@ -2,17 +2,11 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { SharedModule } from 'src/app/shared/share.module'
 import { HomeComponent } from './home.component'
-import { HomeRoutingModule } from './home-routing.module';
-import { NzCarouselModule } from 'ng-zorro-antd/carousel';
-
+import { HomeRoutingModule } from './home-routing.module'
+import { NzCarouselModule } from 'ng-zorro-antd/carousel'
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-    HomeRoutingModule,
-    NzCarouselModule
-  ],
+  imports: [CommonModule, SharedModule, HomeRoutingModule, NzCarouselModule],
   exports: [HomeComponent]
 })
 export class HomeModule {}
--- src/app/features/user/home/models/interface.ts
+++ src/app/features/user/home/models/interface.ts
@@ -1,15 +1,25 @@
 export interface VideoItem {
-    title: string;
-    subTitle: string;
-    thumbnail: string;
-    youtubeUrl: string;
+  title: string
+  subTitle: string
+  thumbnail: string
+  youtubeUrl: string
 }
 
 export interface BannerItem {
-    title?: string;
-    subTitle?: string;
-    buttonText: string;
-    link: string;
-    image: string;
-    overlay?: boolean;
+  title?: string
+  subTitle?: string
+  buttonText: string
+  link: string
+  image: string
+  overlay?: boolean
+}
+
+export interface BrandItem {
+  name: string
+  logo: string
+}
+
+export interface StatsItem {
+  title: string
+  value: string
 }
